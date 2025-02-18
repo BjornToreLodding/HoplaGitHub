77d4a4ef014a6d6b3de708fa0971c0539e997524
Bjorn Tore Lodding <bjortlod@stud.ntnu.no>
2025-02-18 20:04:16 +0100

    ikke viktig

--- Backend/HoplaBackend/obj/Debug/net7.0/HoplaBackend.AssemblyInfo.cs
+++ Backend/HoplaBackend/obj/Debug/net7.0/HoplaBackend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HoplaBackend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+39c9ab74670c9409ccf5db68bc1e997c9ebf117e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b4ec978ed27bbef4d9f42c2b8d3c6026d8f5232c")]
 [assembly: System.Reflection.AssemblyProductAttribute("HoplaBackend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HoplaBackend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
