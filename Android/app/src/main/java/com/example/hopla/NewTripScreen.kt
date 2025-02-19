9d6646158029ce650f04c0afb007d82aee496673
vakvaer <vakvaer@ntnu.no>
2025-02-19 12:49:26 +0100

    Filters in new trip (visuals)

--- Android/app/src/main/java/com/example/hopla/NewTripScreen.kt
+++ Android/app/src/main/java/com/example/hopla/NewTripScreen.kt
@@ -5,10 +5,15 @@ import android.app.Activity
 import android.content.pm.PackageManager
 import android.location.Location
 import android.os.Looper
-import android.util.Log
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
@@ -25,7 +30,8 @@ import com.google.maps.android.compose.GoogleMap
 import com.google.maps.android.compose.MapProperties
 import com.google.maps.android.compose.rememberCameraPositionState
 import com.google.android.gms.maps.model.LatLng
-
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.ui.graphics.Color
 
 @Preview
 @Composable
@@ -37,6 +43,8 @@ fun NewTripScreen() {
     var showDialog by remember { mutableStateOf(false) }
     var tripName by remember { mutableStateOf("") }
     var tripNotes by remember { mutableStateOf("") }
+    var showDropdown by remember { mutableStateOf(false) }
+    var selectedWords by remember { mutableStateOf(listOf<String>()) }
 
     val context = LocalContext.current
     val fusedLocationClient = LocationServices.getFusedLocationProviderClient(context)
@@ -52,36 +60,17 @@ fun NewTripScreen() {
                 if (isRunning) {
                     val newLocation = locationResult.lastLocation
                     if (newLocation != null) {
-                        Log.d(
-                            "NewTripScreen",
-                            "Received location: ${newLocation.latitude}, ${newLocation.longitude}"
-                        )
                         lastLocation?.let {
                             val distanceIncrement = it.distanceTo(newLocation) / 1000.0
                             distance += distanceIncrement
-                            Log.d(
-                                "NewTripScreen",
-                                "Distance increased by $distanceIncrement km, total: $distance km"
-                            )
-                        } ?: run {
-                            Log.d("NewTripScreen", "Initializing lastLocation for first time")
                         }
-
                         lastLocation = newLocation
-                    } else {
-                        Log.e(
-                            "NewTripScreen",
-                            "Location result received but no new location available"
-                        )
                     }
-                } else {
-                    Log.d("NewTripScreen", "Location callback triggered but tracking is stopped")
                 }
             }
         }
     }
 
-    // Ensure permissions are granted before proceeding
     LaunchedEffect(Unit) {
         if (ActivityCompat.checkSelfPermission(
                 context,
@@ -90,13 +79,7 @@ fun NewTripScreen() {
         ) {
             fusedLocationClient.lastLocation.addOnSuccessListener { location ->
                 if (location != null) {
-                    Log.d(
-                        "NewTripScreen",
-                        "Last known location retrieved: ${location.latitude}, ${location.longitude}"
-                    )
                     lastLocation = location
-                } else {
-                    Log.w("NewTripScreen", "No last known location available")
                 }
             }
         } else {
@@ -108,7 +91,6 @@ fun NewTripScreen() {
                 ),
                 1
             )
-            Log.w("NewTripScreen", "Permissions not granted, requesting now")
         }
     }
 
@@ -119,22 +101,17 @@ fun NewTripScreen() {
                     Manifest.permission.ACCESS_FINE_LOCATION
                 ) == PackageManager.PERMISSION_GRANTED
             ) {
-                Log.d("NewTripScreen", "Starting location updates")
                 fusedLocationClient.requestLocationUpdates(
                     locationRequest,
                     locationCallback,
                     Looper.getMainLooper()
                 )
-            } else {
-                Log.e("NewTripScreen", "Location updates requested but permissions are missing")
             }
         } else {
-            Log.d("NewTripScreen", "Stopping location updates")
             fusedLocationClient.removeLocationUpdates(locationCallback)
         }
 
         onDispose {
-            Log.d("NewTripScreen", "Removing location updates on disposal")
             fusedLocationClient.removeLocationUpdates(locationCallback)
         }
     }
@@ -150,7 +127,13 @@ fun NewTripScreen() {
         position = CameraPosition.fromLatLngZoom(LatLng(37.7749, -122.4194), 10f)
     }
 
-    // UI Layout
+    // filter words list
+    val filterWords = listOf(
+        stringResource(R.string.asphalt),
+        stringResource(R.string.gravel),
+        stringResource(R.string.parking)
+    )
+
     Box(modifier = Modifier.fillMaxSize()) {
         GoogleMap(
             modifier = Modifier
@@ -240,26 +223,122 @@ fun NewTripScreen() {
                         value = tripName,
                         onValueChange = { tripName = it },
                         label = { Text(text = stringResource(R.string.trip_name)) },
-                        modifier = Modifier.fillMaxWidth()
-                    )
-                    TextField(
-                        value = tripNotes,
-                        onValueChange = { tripNotes = it },
-                        label = { Text(text = stringResource(R.string.description)) },
-                        modifier = Modifier.fillMaxWidth()
-                    )
-                    Column(
                         modifier = Modifier
                             .fillMaxWidth()
-                            .padding(top = 16.dp)
+                            .padding(bottom = 16.dp)
+                    )
+                    Box(
+                        modifier = Modifier
+                            .height(200.dp)
+                            .verticalScroll(rememberScrollState())
                     ) {
-                        // Add any additional content here
+                        Box(modifier = Modifier.fillMaxSize()) {
+                            TextField(
+                                value = tripNotes,
+                                onValueChange = { tripNotes = it },
+                                label = { Text(text = stringResource(R.string.description)) },
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .height(200.dp)
+                            )
+                            IconButton(
+                                onClick = { /* Handle click */ },
+                                modifier = Modifier
+                                    .align(Alignment.BottomEnd)
+                                    .background(MaterialTheme.colorScheme.primary, shape = CircleShape)
+                                    .padding(4.dp)
+                            ) {
+                                Icon(
+                                    imageVector = Icons.Default.Add,
+                                    contentDescription = null
+                                )
+                            }
+                        }
                     }
+
+                    // Horizontally Scrollable Selected Words Box with Dropdown Icon
+                    Box(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .height(50.dp) // Fixed height for word list
+                            .background(MaterialTheme.colorScheme.secondary)
+                            .padding(horizontal = 8.dp, vertical = 12.dp)
+                    ) {
+                        Row(
+                            modifier = Modifier
+                                .fillMaxSize(),
+                            verticalAlignment = Alignment.CenterVertically,
+                            horizontalArrangement = Arrangement.SpaceBetween // Ensures dropdown icon stays on the right
+                        ) {
+                            // Scrollable Word List
+                            Row(
+                                modifier = Modifier
+                                    .weight(1f) // Makes sure words take up available space
+                                    .padding(end = 8.dp) // Adjust padding for good spacing
+                                    .horizontalScroll(rememberScrollState()),
+                                verticalAlignment = Alignment.CenterVertically
+                            ) {
+                                selectedWords.forEach { word ->
+                                    Box(
+                                        modifier = Modifier
+                                            .background(MaterialTheme.colorScheme.primary)
+                                            .height(36.dp)
+                                            .width(72.dp)
+                                            .padding(end = 8.dp)
+                                            .clickable { selectedWords = selectedWords - word },
+                                        contentAlignment = Alignment.Center
+                                    ) {
+                                        Text(text = word, color = Color.White)
+                                    }
+                                    Spacer(modifier = Modifier.width(8.dp))
+                                }
+                            }
+                            // Dropdown Menu Icon (Fixed on the Right)
+                            IconButton(
+                                onClick = { showDropdown = !showDropdown },
+                                modifier = Modifier
+                                    .size(36.dp) // Adjust size for good spacing
+                                    .background(MaterialTheme.colorScheme.primary, shape = CircleShape)
+                            ) {
+                                Icon(
+                                    imageVector = Icons.Default.Add,
+                                    contentDescription = "Add Word",
+                                )
+                            }
+
+                            // Dropdown Menu
+                            DropdownMenu(
+                                expanded = showDropdown,
+                                onDismissRequest = { showDropdown = false }
+                            ) {
+                                filterWords.forEach { word ->
+                                    val isSelected = word in selectedWords
+
+                                    DropdownMenuItem(
+                                        text = {
+                                            Text(
+                                                text = word,
+                                                color = if (isSelected) Color.Gray else Color.Unspecified // Dim if selected
+                                            )
+                                        },
+                                        onClick = {
+                                            if (!isSelected) {
+                                                selectedWords = selectedWords + word
+                                                showDropdown = false
+                                            }
+                                        },
+                                        enabled = !isSelected // Disable if already selected
+                                    )
+                                }
+                            }
+                        }
+                    }
+
+
                 }
             },
             confirmButton = {
                 Button(onClick = {
-                    // Handle saving trip details here
                     showDialog = false
                     time = 0
                     distance = 0.0
