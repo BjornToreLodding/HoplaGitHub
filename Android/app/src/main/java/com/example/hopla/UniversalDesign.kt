6bf0ccd1014cf76ca90b8e8016efe50cc529b76e
vakvaer <vakvaer@ntnu.no>
2025-03-03 17:27:01 +0100

    Fikse warnings

--- Android/app/src/main/java/com/example/hopla/UniversalDesign.kt
+++ Android/app/src/main/java/com/example/hopla/UniversalDesign.kt
@@ -7,7 +7,6 @@ import android.graphics.BitmapFactory
 import android.widget.Toast
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
-import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
@@ -49,7 +48,6 @@ import androidx.compose.runtime.snapshots.SnapshotStateList
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.graphics.SolidColor
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
