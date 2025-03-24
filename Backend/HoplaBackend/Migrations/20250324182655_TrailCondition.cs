8755d5ca767360607d63d168b56d5b405af28d71
bjortlod <bjortlod@stud.ntnu.no>
2025-03-24 19:28:01 +0100

    GET /trails/updates og enum-fix med migrasjon?

---
+++ Backend/HoplaBackend/Migrations/20250324182655_TrailCondition.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HoplaBackend.Migrations
+{
+    /// <inheritdoc />
+    public partial class TrailCondition : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Rating",
+                schema: "public",
+                table: "TrailReviews",
+                newName: "Condition");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Condition",
+                schema: "public",
+                table: "TrailReviews",
+                newName: "Rating");
+        }
+    }
+}
