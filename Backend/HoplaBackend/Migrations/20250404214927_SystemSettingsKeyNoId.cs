53f92c2c00ac9c592425eeca9e29332c7bbdd5d0
bjortlod <bjortlod@stud.ntnu.no>
2025-04-04 23:52:33 +0200

    migrasjon av key

---
+++ Backend/HoplaBackend/Migrations/20250404214927_SystemSettingsKeyNoId.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace HoplaBackend.Migrations
+{
+    /// <inheritdoc />
+    public partial class SystemSettingsKeyNoId : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_SystemSettings",
+                schema: "public",
+                table: "SystemSettings");
+
+            migrationBuilder.DropColumn(
+                name: "Id",
+                schema: "public",
+                table: "SystemSettings");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_SystemSettings",
+                schema: "public",
+                table: "SystemSettings",
+                column: "Key");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_SystemSettings",
+                schema: "public",
+                table: "SystemSettings");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Id",
+                schema: "public",
+                table: "SystemSettings",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0)
+                .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_SystemSettings",
+                schema: "public",
+                table: "SystemSettings",
+                column: "Id");
+        }
+    }
+}
